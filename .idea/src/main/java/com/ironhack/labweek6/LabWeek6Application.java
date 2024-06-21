c261726af41ec02cf14f7950ad15bb3fa95f7d5b
Acer <plamen0141@gmail.com>
2024-06-21 19:39:47 +0200

    LabWeek6 done!

---
+++ .idea/src/main/java/com/ironhack/labweek6/LabWeek6Application.java
@@ -0,0 +1,13 @@
+package com.ironhack.labweek6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWeek6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabWeek6Application.class, args);
+	}
+
+}
---
+++ .idea/src/test/java/com/ironhack/labweek6/LabWeek6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.labweek6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabWeek6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
